d18e78cfeaa303cfecdeccbe70915dfbef1fea69
jeffmorrisjr <jeff@zaarly.com>
2015-02-21 19:28:47 -0800

    fixed note method

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,8 +1,8 @@
 class NotesController < ApplicationController
 	before_action :find_note, only: [:show, :edit, :update, :destroy]
-	def index 
-		@notes = Note.all.order("created_at DESC")
 
+	def index
+		@notes = Note.all.order("created_at DESC")
 	end
 
 	def show
@@ -10,15 +10,15 @@ class NotesController < ApplicationController
 
 	def new
 		@note = Note.new
-	end 
+	end
 
 	def create
 		@note = Note.new(note_params)
 
 		if @note.save
-		  redirect_to @note
+			redirect_to @note
 		else
-		  render 'new'
+			render 'new'
 		end
 	end
 
@@ -40,5 +40,4 @@ class NotesController < ApplicationController
 	def note_params
 		params.require(:note).permit(:title, :content)
 	end
-
 end
